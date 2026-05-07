688f0ccd873319bb8e9ddef1878b6e7812a4ed6e
amannaryal5-art <amannaryal5@gmail.com>
2026-05-07 11:41:30 +0530

    d

--- threadsapp-admin/next.config.js
+++ threadsapp-admin/next.config.js
@@ -1,27 +1,23 @@
-experimental: {
-  outputFileTracingExcludes: {
-    '*': ['**/(dashboard)/page_client-reference-manifest.js'],
-  },
-},
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/typescript",
+        "node_modules/prettier",
+        "**/(dashboard)/page_client-reference-manifest.js",
+      ],
     },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    experimental: {
-      outputFileTracingExcludes: {
-        "*": [
-          "node_modules/@swc/core-linux-x64-gnu",
-          "node_modules/@swc/core-linux-x64-musl",
-          "node_modules/typescript",
-          "node_modules/prettier",
-        ],
-      },
-    },
-  };
-  
-  module.exports = nextConfig;
+  },
+};
+
+module.exports = nextConfig;
